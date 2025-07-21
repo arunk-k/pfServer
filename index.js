3cd18249a348afa0bdde163206774b6f13f4e2b9
Arun <arunkk1872002@gmail.com>
2025-07-21 10:19:25 +0530

    third commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require("dotenv").config()//configuring dotenv package to server for loading env variables to process.env
 
-const express=require('express')//importing express
+const express = require('express')//importing express
 
-const cors=require('cors')
+const cors = require('cors')
 
-const router=require('./routes/routes')
+const router = require('./routes/routes')
 
 require('./connect/dbConnect')
 
@@ -16,9 +16,9 @@ server.use(express.json())//configuring json middleware to convert json data for
 
 server.use(router)
 
-server.use('/image',express.static('./uploads'))
+server.use('/image', express.static('./uploads'))
 
-const PORT = 3000 || process.env.PORT//setting default port number and alternate port number from env
+const PORT = process.env.PORT || 3000//setting default port number and alternate port number from env
 
 server.listen(PORT, () => {//for making server run and wait for requests
     console.log("Server Running at", PORT);
@@ -29,23 +29,23 @@ server.listen(PORT, () => {//for making server run and wait for requests
 //     res.send("Request Hit!!")
 // })
 
-// server.get('/getreq/:uid', (req, res) => {//get request handler 
+// server.get('/getreq/:uid', (req, res) => {//get request handler
 //     console.log(req.params.uid);
 //     res.send("Get Request Hit!!")
 // })
 
-// server.post('/postreq', (req, res) => {//post request handler 
+// server.post('/postreq', (req, res) => {//post request handler
 //     console.log(req.query);
 //     console.log(req.body);
 //     // res.send("Post Request Hit!!")
 //     res.json({"req":"Ok"}).status(200)
 // })
 
-// server.put('/putreq', (req, res) => {//put request handler 
+// server.put('/putreq', (req, res) => {//put request handler
 //     res.send("PUT Request Hit!!")
 // })
 
-// server.delete('/deletereq', (req, res) => {//delete request handler 
+// server.delete('/deletereq', (req, res) => {//delete request handler
 //     res.send("Delete Request Hit!!")
 // })
 
